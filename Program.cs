87c3e07aa8a3b1a07e35763dfc5d0656a1664415
SzBalesz <baleszvagyok7000@gmail.com>
2023-03-08 16:58:55 +0100

    Fájl létrehozva

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bloodpressure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
